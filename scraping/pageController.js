451bb1bcd62b5e75f7b4be810b20e85dba32e6e5
cemgulec <cem.ggulecc@gmail.com>
2022-05-02 15:50:07 +0200

    able to get innertexts | need further development for links and such

---
+++ scraping/pageController.js
@@ -0,0 +1,15 @@
+const pageScraper = require('./pageScraper');
+async function scrapeAll(browserInstance){
+	let browser;
+	try{
+		browser = await browserInstance;
+		let pageContent = await pageScraper.scraper(browser);	
+		
+		console.log(pageContent)
+	}
+	catch(err){
+		console.log("Could not resolve the browser instance => ", err);
+	}
+}
+
+module.exports = (browserInstance) => scrapeAll(browserInstance)
---
+++ scraping/pageScraper.js
@@ -0,0 +1,33 @@
+const scraperObject = {
+    url: 'https://cellxgene.cziscience.com/',
+
+    async scraper(browser){
+		let regexForLink = /href=\\["']([^"']*)["']/
+		let page = await browser.newPage();
+
+		console.log(`Navigating to ${this.url}...`);
+
+		// Navigate to the selected page
+		await page.goto(this.url);
+
+		// Wait for the required DOM to be rendered
+		await page.waitForSelector('.bp3-button-text');		  
+
+		// Get the link to all the required books
+		let pageContent = await page.evaluate(() => {
+			// Extract the links from the data
+			const tds = Array.from(document.querySelectorAll('table tbody tr td'))
+			const url = tds.map(td => td.innerText)
+			console.log(JSON.stringify(tds.map(td => td.innerHTML)))
+    		return url
+		});
+
+		// Firstly convert object into string format
+		// Then parse it by [""] character set to separate each dataset info
+		pageContent = JSON.stringify(pageContent).split(/""/)
+
+		return pageContent
+    }
+}
+
+module.exports = scraperObject;
