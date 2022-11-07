7c503873dfa187fb97bd8b29b11e79443d7a4b9e
cemgulec <cem.ggulecc@gmail.com>
2022-11-07 17:12:27 +0100

    Update sfaira_collab.py

--- Data-Descriptor/Cell-Level/sfaira-collab/sfaira_collab.py
+++ Data-Descriptor/Cell-Level/sfaira-collab/sfaira_collab.py
@@ -7,20 +7,22 @@ from sfaira.data.dataloaders.databases import DatasetSuperGroupDatabases
 # TODO:
 # list collections
 # list datasets
-# 
+# arguments to be taken from terminal
 
-COLLECTION_ID = sys.argv[1]
+#COLLECTION_ID = sys.argv[1]
 
-cache_path = os.path.join(".", "data")
-dsg = sfaira.data.dataloaders.databases.DatasetSuperGroupDatabases(data_path = cache_path, cache_metadata = True)
+if __name__ == "__main__":
+    cache_path = os.path.join(".", "data")
+    dsg = DatasetSuperGroupDatabases(data_path = cache_path, cache_metadata = True)
 
-#collecting each collection from dsg object
-collections = [x.collection_id for x in dsg.dataset_groups]
+    #collecting each collection from dsg object
+    collections = [x.collection_id for x in dsg.dataset_groups]
+    COLLECTION_ID = collections[0]
 
-# splitting into datasets
-dsg.subset(key = "collection_id", values = COLLECTION_ID)
+    # splitting into datasets
+    dsg.subset(key = "collection_id", values = COLLECTION_ID)
 
-dsg.download()
-dsg.load()
-adata = dsg.adata_ls
-print(adata)
+    dsg.download()
+    dsg.load()
+    adata = dsg.adata_ls
+    print(adata)
