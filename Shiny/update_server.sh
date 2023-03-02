ea7badea771f136ff7d950845fee6cfab774b720
Cem Güleç <cem.ggulecc@gmail.com>
2023-03-02 01:12:24 +0100

    Create update_server.sh

---
+++ Shiny/update_server.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+REPO_URL="https://raw.githubusercontent.com"
+REPO_OWNER="heiniglab"
+REPO_NAME="scPower"
+BRANCH_NAME="master"
+UI_GIT_PATH="inst/shinyApp/ui.R"
+UI_LOCAL_PATH="ui.R"
+UI_REMOTE_PATH="ui_remote.R"
+SERVER_GIT_PATH="inst/shinyApp/server.R"
+SERVER_LOCAL_PATH="server.R"
+SERVER_REMOTE_PATH="server_remote.R"
+
+# download both the ui and server codes as a remote file
+curl -o "${UI_REMOTE_PATH}" "${REPO_URL}/${REPO_OWNER}/${REPO_NAME}/${BRANCH_NAME}/${UI_GIT_PATH}"
+
+# compare the remote and local files for the ui script
+if diff -q "${UI_REMOTE_PATH}" "${UI_LOCAL_PATH}" >/dev/null 2>&1; then
+  echo "No changes found for ui.R. Checking server.R file."
+else
+  echo "Changes found and listed below. Updating ui local file..."
+  diff -u "${UI_REMOTE_PATH}" "${UI_LOCAL_PATH}" | grep ^[+-]
+  cp "${UI_REMOTE_PATH}" "${UI_LOCAL_PATH}"
+fi
+
+# compare the remote and local files for the server script
+
+# then finally remove both the ui and server remote files
+rm "${UI_REMOTE_PATH}"
