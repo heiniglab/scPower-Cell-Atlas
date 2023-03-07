3450af3ca1240617f480525ef1fd3e1dd9d22b73
cemgulec <cem.ggulecc@gmail.com>
2023-03-07 23:54:42 +0100

    Update update_server.sh

--- Shiny/update_server.sh
+++ Shiny/update_server.sh
@@ -13,6 +13,7 @@ SERVER_REMOTE_PATH="server_remote.R"
 
 # download both the ui and server codes as a remote file
 curl -o "${UI_REMOTE_PATH}" "${REPO_URL}/${REPO_OWNER}/${REPO_NAME}/${BRANCH_NAME}/${UI_GIT_PATH}"
+curl -o "${SERVER_REMOTE_PATH}" "${REPO_URL}/${REPO_OWNER}/${REPO_NAME}/${BRANCH_NAME}/${SERVER_GIT_PATH}"
 
 # compare the remote and local files for the ui script
 if diff -q "${UI_REMOTE_PATH}" "${UI_LOCAL_PATH}" >/dev/null 2>&1; then
@@ -24,6 +25,14 @@ else
 fi
 
 # compare the remote and local files for the server script
+if diff -q "${SERVER_REMOTE_PATH}" "${SERVER_LOCAL_PATH}" >/dev/null 2>&1; then
+  echo "No changes found for server.R"
+else
+  echo "Changes found and listed below. Updating server local file..."
+  diff -u "${UI_REMOTE_PATH}" "${UI_LOCAL_PATH}" | grep ^[+-]
+  cp "${SERVER_REMOTE_PATH}" "${SERVER_LOCAL_PATH}"
+fi
 
 # then finally remove both the ui and server remote files
 rm "${UI_REMOTE_PATH}"
+rm "${SERVER_REMOTE_PATH}"
