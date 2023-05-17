12275e60e80f193b3cb65b6d729a07659aed4bf7
cemgulec <cem.ggulecc@gmail.com>
2023-05-17 15:40:25 +0200

    Create check_error_messages.py

---
+++ Experimentation/check_error_messages.py
@@ -0,0 +1,31 @@
+import re
+
+text = """
+"completeDatasetID" "idToName" "errorMessage" "datasetBodySpecific" "cellCount"
+"EFO:0011025_UBERON:0000178_CL:0000897" "10x 5' v1_blood_CD4-positive, alpha-beta memory T cell" "Error in parametricDispersionFit_DEseq(means, disps): Parametric dispersion fit failed.
+" "1_1_26" 3276
+
+
+"completeDatasetID" "idToName" "errorMessage" "datasetBodySpecific" "cellCount"
+"EFO:0011025_UBERON:0000178_CL:0000816" "10x 5' v1_blood_immature B cell" "Error in parametricDispersionFit_DEseq(means, disps): Parametric dispersion fit failed.
+" "1_1_26" 255
+
+
+"completeDatasetID" "idToName" "errorMessage" "datasetBodySpecific" "cellCount"
+"EFO:0011025_UBERON:0000178_CL:0000775" "10x 5' v1_blood_neutrophil" "Error in parametricDispersionFit_DEseq(means, disps): Parametric dispersion fit failed.
+" "1_1_26" 299
+"""
+
+# Regular expression pattern to find the error message between double quotes
+pattern = r'"(Error in .+?)"'
+
+# Search for the pattern in the text
+matches = re.findall(pattern, text, re.DOTALL)
+
+# Print the error messages
+for match in matches:
+    print(match)
+
+
+# for unique error messages
+print(list(set(matches)))
