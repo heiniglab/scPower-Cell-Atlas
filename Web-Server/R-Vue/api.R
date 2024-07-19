4a0d14ca394cc8f72720f909dca78f836b53e96d
cemgulec <cem.ggulecc@gmail.com>
2024-07-19 12:19:50 +0200

    simple test files and broadcasting

--- Web-Server/R-Vue/api.R
+++ Web-Server/R-Vue/api.R
@@ -1,21 +1,10 @@
 library(plumber)
+library(jsonlite)
 
-#* @apiTitle scPower API
+data <- readRDS("scPower_shiny/power_study_plot.rds")
 
-#* Return the dataset as JSON
-#* @param dataset The name of the dataset
+#* @apiTitle RDS Data API
 #* @get /data
-# example usage: http://localhost:8000/data?dataset=iris
-function(dataset) {
-  data <- get(dataset, "package:datasets")
-  as.data.frame(data)
-}
-
-# Enable CORS
-#* @filter cors
-function(res) {
-  res$setHeader("Access-Control-Allow-Origin", "*")
-  res$setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-  res$setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-  plumber::forward()
+function() {
+  return(toJSON(data))
 }
