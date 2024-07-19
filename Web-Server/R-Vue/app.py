40eb9857a745a64725f12fb47995571d41d96800
cemgulec <cem.ggulecc@gmail.com>
2024-07-19 17:06:24 +0200

    proper success and error messages

--- Web-Server/R-Vue/app.py
+++ Web-Server/R-Vue/app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import requests
 import json
-import io
+import time
+import os
 
 def fetch_api_data(api_url):
     try:
         response = requests.get(api_url)
         response.raise_for_status()  # Raise an exception for bad status codes
+        st.success("Successfully fetched the data. Loading it...")
+        time.sleep(2)    
         return response.json()
     except requests.RequestException as e:
         st.error(f"API Error: {str(e)}")
@@ -18,6 +21,8 @@ def fetch_api_data(api_url):
 def read_json_file(file):
     try:
         content = file.getvalue().decode("utf-8")
+        st.success("File successfully uploaded and validated as JSON...")
+        time.sleep(2)
         return json.loads(content)
     except json.JSONDecodeError:
         st.error("The uploaded file is not valid JSON.")
@@ -27,7 +32,7 @@ def main():
     st.title("scPower Power Results")
     api_url = "http://localhost:8000/data"
     
-    uploaded_file = st.file_uploader("Choose a JSON file", type=['json'])
+    uploaded_file = st.file_uploader("Choose a file to upload")
 
     if st.button("Fetch Data"):
         if uploaded_file is None:
@@ -35,7 +40,12 @@ def main():
             data = fetch_api_data(api_url)
         else:
             print("read file")
-            data = read_json_file(uploaded_file)
+            file_extension = os.path.splitext(uploaded_file.name)[1].lower()
+
+            if file_extension == ".json":
+                data = read_json_file(uploaded_file)
+            else:
+                st.error("Error: The uploaded file is not a valid JSON file.")
 
         if data is not None:
             st.subheader("Power Results:")
